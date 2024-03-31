5f849be00734096692b37f8f8d9edc14cc4e3e3b
iamzubair6 <zubair.rahman89@gmail.com>
2024-03-31 09:40:54 +0600

    Dynamic data intigration

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,6 +6,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/currencyFormater";
 interface CardsType {
   title: string;
   description: string | number;
@@ -17,7 +18,7 @@ async function GetSalesData() {
     _count: true,
   });
 
-  let amount = salesData?._sum?.pricePaidInTaka || 0;
+  let amount = 1000000;
   let numberOfSales = salesData?._count || 0;
 
   return {
@@ -25,23 +26,44 @@ async function GetSalesData() {
     numberOfSales,
   };
 }
+async function GetUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({ _sum: { pricePaidInTaka: true } }),
+  ]);
+
+  let averageValuePerUser =
+    userCount === 0 ? 0 : (orderData?._sum?.pricePaidInTaka || 0) / userCount;
+
+  return {
+    userCount,
+    averageValuePerUser,
+  };
+}
 
 export default async function AdminDashboard() {
-  const sales = await GetSalesData();
+  const [sales, users] = await Promise.all([GetSalesData(), GetUserData()]);
+
   const cards: CardsType[] = [
     {
       title: "Sales",
-      description: sales?.numberOfSales.toLocaleString("bn-BD"),
+      description: sales?.numberOfSales.toLocaleString("bn-BD") + " Orders",
       content: sales?.amount.toLocaleString("bn-BD", {
         style: "currency",
         currency: "BDT",
+        minimumFractionDigits: 0,
       }),
     },
     {
-      title: "Orders",
-      description: "Lorem ipsum dolor sit amet.",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, atque.",
+      title: "Customers",
+      // description: users?.userCount.toLocaleString("bn-BD") + " Users",
+      // content: users?.averageValuePerUser.toLocaleString("bn-BD", {
+      //   style: "currency",
+      //   currency: "BDT",
+      //   minimumFractionDigits: 0,
+      // }),
+      description: formatNumber(users?.userCount) + " " + "Users",
+      content: formatCurrency(users?.averageValuePerUser),
     },
     {
       title: "Products",
