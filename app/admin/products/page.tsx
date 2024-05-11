a97d85d31ff139b7bb0f7d473089b694eb38049e
iamzubair6 <zubair.rahman89@gmail.com>
2024-05-11 23:56:05 +0600

    Product table created

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
 import PageHeader from "../_components/PageHeader";
 
 export default function AdminProductsPage() {
   return (
     <>
-      <PageHeader>Products</PageHeader>
+      <div className="flex justify-between gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href={"/admin/products/new"}>Add New Product</Link>
+        </Button>
+      </div>
+      <ProductTable />
     </>
+  );
+}
+function ProductTable() {
+  //dasd
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Order</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Action</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+    </Table>
   );
 }
