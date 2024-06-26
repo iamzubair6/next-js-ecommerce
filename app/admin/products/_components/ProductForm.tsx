a7ada5e5d150d82aeb81ca83f9958a25b76d7d57
iamzubair6 <zubair.rahman89@gmail.com>
2024-06-26 23:44:35 +0600

    ProductForm page complete, action -> addProduct action added and zod package install

--- app/admin/products/_components/ProductForm.tsx
+++ app/admin/products/_components/ProductForm.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+import { addProduct } from "../../_action/products";
 
 const ProductForm = () => {
   const [price, setPrice] = useState<number>();
   return (
-    <form className="grid grid-cols-2 gap-4">
+    <form action={addProduct} className="grid grid-cols-2 gap-4">
       <div className="space-y-2">
         <Label className="text-lg text-black" htmlFor="name">
           Product Name
@@ -33,6 +36,32 @@ const ProductForm = () => {
           placeholder="Enter Product Price"
         />
       </div>
+      <div className="space-y-2 col-span-full">
+        <Label className="text-lg text-black" htmlFor="description">
+          Description
+        </Label>
+        <Textarea
+          id="descritption"
+          name="description"
+          required
+          placeholder="Enter Description"
+        />
+      </div>
+      <div className="space-y-2">
+        <Label className="text-lg text-black" htmlFor="file">
+          File
+        </Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label className="text-lg text-black" htmlFor="image">
+          Image
+        </Label>
+        <Input type="file" id="image" name="image" required accept="image/*" />
+      </div>
+      <Button type="submit" className="col-span-full">
+        Save
+      </Button>
     </form>
   );
 };
