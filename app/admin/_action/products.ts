bbd42a2c1d9c759efe3e35da3092efd9a44888f0
iamzubair6 <zubair.rahman89@gmail.com>
2024-06-29 02:15:18 +0600

    ProductForm page complete and action -> add product action added db mutation complete

--- app/admin/_action/products.ts
+++ app/admin/_action/products.ts
@@ -1,5 +1,50 @@
 "use server";
 
-export async function addProduct(formData: FormData) {
-  console.log(formData, "formDataformData");
+import db from "@/db/db";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+const fileSchema = z.instanceof(File, { message: "File is required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
+const addSchema = z.object({
+  product_name: z.string().min(1),
+  product_price: z.coerce.number().int().min(1),
+  description: z.string().min(1),
+  file: fileSchema.refine((file) => file.size > 0, {
+    message: "File is required",
+  }),
+  image: imageSchema.refine((file) => file.size > 0, {
+    message: "Image is required",
+  }),
+});
+
+export async function addProduct(prevState: unknown, formData: FormData) {
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
+  }
+  const data = result.data;
+  await fs.mkdir("products", { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.writeFile(
+    `public${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer())
+  );
+  await db.product.create({
+    data: {
+      isAvailableForPurchase: false,
+      name: data.product_name,
+      priceInTaka: data.product_price,
+      description: data.description,
+      filePath,
+      imagePath,
+    },
+  });
+  redirect("/admin/products");
 }
--- app/admin/products/_components/ProductForm.tsx
+++ app/admin/products/_components/ProductForm.tsx
@@ -3,13 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { LoaderIcon } from "lucide-react";
 import { useState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
 import { addProduct } from "../../_action/products";
 
 const ProductForm = () => {
+  const [error, action] = useFormState(addProduct, {});
   const [price, setPrice] = useState<number>();
   return (
-    <form action={addProduct} className="grid grid-cols-2 gap-4">
+    <form action={action} className="grid grid-cols-2 gap-4">
       <div className="space-y-2">
         <Label className="text-lg text-black" htmlFor="name">
           Product Name
@@ -21,6 +24,9 @@ const ProductForm = () => {
           required
           placeholder="Enter Product Name"
         />
+        {error?.product_name && (
+          <div className="text-destructive">{error?.product_name}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label className="text-lg text-black" htmlFor="price">
@@ -35,6 +41,9 @@ const ProductForm = () => {
           required
           placeholder="Enter Product Price"
         />
+        {error?.product_price && (
+          <div className="text-destructive">{error?.product_price}</div>
+        )}
       </div>
       <div className="space-y-2 col-span-full">
         <Label className="text-lg text-black" htmlFor="description">
@@ -46,24 +55,36 @@ const ProductForm = () => {
           required
           placeholder="Enter Description"
         />
+        {error?.description && (
+          <div className="text-destructive">{error?.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label className="text-lg text-black" htmlFor="file">
           File
         </Label>
         <Input type="file" id="file" name="file" required />
+        {error?.file && <div className="text-destructive">{error?.file}</div>}
       </div>
       <div className="space-y-2">
         <Label className="text-lg text-black" htmlFor="image">
           Image
         </Label>
         <Input type="file" id="image" name="image" required accept="image/*" />
+        {error?.image && <div className="text-destructive">{error?.image}</div>}
       </div>
-      <Button type="submit" className="col-span-full">
-        Save
-      </Button>
+      <SubmitButton />
     </form>
   );
 };
 
 export default ProductForm;
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" className="col-span-full" disabled={pending}>
+      {pending ? <LoaderIcon className="animate-spin" /> : "Save"}
+    </Button>
+  );
+};
